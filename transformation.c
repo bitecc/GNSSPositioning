3bdda31f3325e0029c1b0094ea0150bee1ba96f0
JoshuaChen <2253059449@qq.com>
2020-04-24 16:54:59 +0800

    position

--- transformation.c
+++ transformation.c
@@ -1,4 +1,7 @@
+#include<stdio.h>
+#include<math.h>
 #include"transformation.h"
+const double PI = 3.1415926535897932384626433832795;
 
 /****************************************************************************
   COMMON2MJD
@@ -13,10 +16,8 @@
 ****************************************************************************/
 void COMMON2MJD(struct COMMONTIME* ctime, struct MJDTIME* mjd)
 {
-    unsigned short y;
-    unsigned short m;
-    int JDDays;
-    double JDFracDay;
+    int y;
+    int m;
     if (ctime->Month <= 2)
     {
         y = ctime->Year - 1;
@@ -27,10 +28,8 @@ void COMMON2MJD(struct COMMONTIME* ctime, struct MJDTIME* mjd)
         y = ctime->Year;
         m = ctime->Month;
     }
-    JDDays = (int)(365.25 * y) + (int)(30.6001 * (m + 1)) + ctime->Day + 1720981;
-    JDFracDay = ((double)ctime->Hour + ctime->Minute / 60.0 + ctime->Second / 3600) / 24+0.5;
-    mjd->Days = JDDays-2400000;
-    mjd->FracDay = JDFracDay-0.5;
+    mjd->Days = (int)(365.25 * y) + (int)(30.6001 * ((double)m + 1)) + ctime->Day -679019;
+    mjd->FracDay = ((double)ctime->Hour + ctime->Minute / 60.0 + ctime->Second / 3600) / 24;
 }
 
 /****************************************************************************
@@ -46,20 +45,11 @@ void COMMON2MJD(struct COMMONTIME* ctime, struct MJDTIME* mjd)
 ****************************************************************************/
 void MJD2COMMON(struct MJDTIME* mjd, struct COMMONTIME* ctime)
 {
-    int JDDays;
-    double JDFracDay;
-    JDDays = mjd->Days + 2400000;
-    JDFracDay = mjd->FracDay + 0.5;
-    if (JDFracDay >= 1)
-    {
-        JDFracDay -= 1;
-        JDDays += 1;
-    }
-    int a = (int)(JDDays + JDFracDay + 0.5);
+    int a = (int)(mjd->Days + 2400001);
     int b = a + 1537;
     int c = (int)((b - 122.1) / 365.25);
     int d = (int)(c * 365.25);
-    int e = (int)((b - d) / 30.6001);
+    int e = (int)(((double)b - (double)d) / 30.6001);
     ctime->Day = b - d - (int)(30.60001 * e);
     ctime->Month = e - 1 - 12 * (int)(e / 14);
     ctime->Year = c - 4715 - (int)((7 + ctime->Month) / 10);
@@ -82,7 +72,7 @@ void MJD2COMMON(struct MJDTIME* mjd, struct COMMONTIME* ctime)
 void MJD2GPST(struct MJDTIME* mjd, struct GPSTIME* gpst)
 {
     gpst->Week = (int)((mjd->Days + mjd->FracDay - 44244) / 7);
-    gpst->SecOfWeek = (mjd->Days + mjd->FracDay - 44244 - gpst->Week * 7) * 86400;
+    gpst->SecOfWeek = (mjd->Days + mjd->FracDay - 44244 - (double)gpst->Week * 7) * 86400;
 }
 
 /****************************************************************************
@@ -136,4 +126,62 @@ void GPST2COMMON(struct GPSTIME* gpst, struct COMMONTIME* ctime)
     struct MJDTIME mjd = { 0,0 };
     GPST2MJD(gpst, &mjd);
     MJD2COMMON(&mjd, ctime);
+}
+
+/****************************************************************************
+  BLh2XYZ
+
+  目的：大地坐标转换为笛卡尔坐标
+
+  编号：02011
+
+  参数：
+  blh       大地坐标，输入结构体的指针
+  xyz       笛卡尔坐标，输出结构体的指针
+****************************************************************************/
+void BLh2XYZ(struct BLh* blh, struct XYZ* xyz)
+{
+    double a = 6378137.0; double f = 1.0 / 298.257223563;
+    double D2R = PI / 180.0;
+    double e2 = f * 2 - f * f;
+    double N = a / sqrt(1 - e2 * pow(sin(blh->B * D2R), 2));
+    xyz->X = (N + blh->H) * cos(blh->B * D2R) * cos(blh->L * D2R);
+    xyz->Y = (N + blh->H) * cos(blh->B * D2R) * sin(blh->L * D2R);
+    xyz->Z = (N * (1 - e2) + blh->H) * sin(blh->B * D2R);
+}
+
+/****************************************************************************
+  XYZ2BLh
+
+  目的：笛卡尔坐标转换为大地坐标
+
+  编号：02012
+
+  参数：
+  xyz       笛卡尔坐标，输入结构体的指针
+  blh       大地坐标，输出结构体的指针
+****************************************************************************/
+void XYZ2BLh(struct XYZ* xyz, struct BLh* blh)
+{
+    double R2D = 180.0 / PI;
+    double a = 6378137.0; double f = 1.0 / 298.257223563;
+    double e2 = f * 2 - f * f;
+    double R2 = pow(xyz->X, 2) + pow(xyz->Y, 2);
+    blh->L = atan2(xyz->Y, xyz->X) * R2D;
+    double dZ = e2 * xyz->Z;
+    double dZold = dZ;
+    double SB = 0;
+    double N = 0;
+    while (1) 
+    {
+        SB = (xyz->Z + dZ) / sqrt(R2 + pow(xyz->Z + dZ, 2));
+        N = a / sqrt(1 - e2 * SB*SB);
+        dZ = N * e2 * SB;
+        if (fabs(dZ - dZold) < 1e-6)
+            break;
+        else
+            dZold = dZ;
+    }
+    blh->B = atan2(xyz->Z + dZ, sqrt(R2)) * R2D;
+    blh->H = sqrt(R2 + pow(xyz->Z + dZ, 2)) - N;
 }
--- transformation.h
+++ transformation.h
@@ -33,4 +33,24 @@ void MJD2GPST(struct MJDTIME* mjd, struct GPSTIME* gpst);
 void GPST2MJD(struct GPSTIME* gpst, struct MJDTIME* mjd);
 void COMMON2GPST(struct COMMONTIME* ctime, struct GPSTIME* gpst);
 void GPST2COMMON(struct GPSTIME* gpst, struct COMMONTIME* ctime);
+
+/* 笛卡尔坐标 */
+struct XYZ
+{
+	double X;
+	double Y;
+	double Z;
+};
+
+/* 大地坐标 */
+struct BLh
+{
+	double L;//经度
+	double B;//纬度
+	double H;
+};
+
+void BLh2XYZ(struct BLh* blh, struct XYZ* xyz);
+void XYZ2BLh(struct XYZ* xyz, struct BLh* blh);
+
 #endif
